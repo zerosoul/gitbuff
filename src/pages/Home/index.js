dff56a4429d045a5e6d8de5be358ceefcdf61076
zerosoul <yanggc888@163.com>
2020-12-09 19:48:31 +0800

    feat: first blood

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import { useState } from 'react';
 import StyledWrapper from './styled';
 export default function Home() {
+  const [output, setOnput] = useState('');
+  const [tip, setTip] = useState('');
+  const handleInputChange = (evt) => {
+    let { value } = evt.target;
+
+    try {
+      let tmpUrl = new URL(value);
+      const { pathname, search, hash } = tmpUrl;
+      console.log({ tmpUrl });
+      setOnput(`https://hub.fastgit.org${pathname}${search}${hash}`);
+      if (tmpUrl.hostname != 'github.com') {
+        setTip('非Github地址');
+      } else {
+        setTip('');
+      }
+    } catch (error) {
+      console.log({ error });
+    }
+  };
   return (
     <StyledWrapper>
-      <a
-        className="link"
-        href="https://github.com/zerosoul/react-starter"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Read The Fucking Docs!
+      {tip && <div className="tip">{tip}</div>}
+      <input
+        onChange={handleInputChange}
+        className="input"
+        type="url"
+        placeholder="输入Github地址"
+      />
+
+      <a download href={output} className={`btn ${output ? 'downloadable' : ''}`}>
+        跳转/下载
       </a>
     </StyledWrapper>
   );
--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -2,11 +2,40 @@ import styled from 'styled-components';
 
 const StyledWrapper = styled.section`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
-  .link {
-    font-size: 1.4rem;
+  .tip {
+    font-size: 2rem;
+    position: fixed;
+    top: 1rem;
+    left: 50%;
+    transform: translateX(-50%);
+    padding: 1rem 1.2rem;
+    color: #fff;
+    background-color: rgba(2, 2, 2, 0.8);
+  }
+  .input {
+    margin-top: -4rem;
+    min-width: 38rem;
+    font-size: 1.6rem;
+    padding: 1.2rem;
+    border-radius: 0.8rem;
+    border: 2px solid #333;
+    &:focus {
+      outline: none;
+      border-color: blue;
+    }
+  }
+  .btn {
+    font-size: 2rem;
+    padding: 1rem 1.2rem;
+    text-decoration: none;
+    visibility: hidden;
+    &.downloadable {
+      visibility: visible;
+    }
   }
 `;
 
